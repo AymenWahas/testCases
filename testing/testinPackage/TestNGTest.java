6060449a8a3730f855c36b6cace355d12d65d1e0
AymenWahas <91993439+AymenWahas@users.noreply.github.com>
2022-04-06 22:23:01 +0300

    Add files via upload

---
+++ testing/testinPackage/TestNGTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+/**
+ * Created by hp on 6/4/2022.
+ */
+public class TestNGTest {
+    @Test
+    public void f1() throws Exception {
+
+    }
+
+    @Test
+    public void beforeMethod2() throws Exception {
+
+    }
+
+    @Test
+    public void afterMethod() throws Exception {
+
+    }
+
+    @Test
+    public void beforeMethod() throws Exception {
+
+    }
+
+    @Test
+    public void f() throws Exception {
+
+    }
+
+    @Test
+    public void beforeMethod1() throws Exception {
+
+    }
+
+    @Test
+    public void afterMethod1() throws Exception {
+
+    }
+
+}
